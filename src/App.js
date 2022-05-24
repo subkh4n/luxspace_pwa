90b021122b603b5981e6ecb6956bb7c21a178b89
Moh Subhan <subkh4n@gmail.com>
2022-05-24 14:24:18 +0700

    ketiga

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="Font-Bold text-red">Haloo </h1>
+    <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
+      <p className="text-3xl text-red-700 font-bold mb-5">Welcome!</p>
+      <p className="text-gray-500 text-lg">React and Tailwind CSS in action</p>
     </div>
   );
 }
